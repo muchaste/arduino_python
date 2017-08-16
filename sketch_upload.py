116301df90a0ea818eaba8e21bd592b6ea164462
Stefan Mucha <stefan.mucha@hu-berlin.de>
2017-08-16 17:03:40 +0200

    latest version of python skript to upload sketches to the arduino via the windows cmd.exe

--- sketch_upload.py
+++ sketch_upload.py
@@ -2,11 +2,14 @@
 
 import subprocess
 import win32com.client
+import serial
 
 # Enter the basic settings here
 arduino_path    = "C:\\Program Files (x86)\\Arduino"    # Path to the arduino IDE
 action          = " --upload "                          # Command for IDE (--upload or --verify)
-filepath        = '"C:\\Users\\Stefan\ Mucha\\arduino_python\\tentacle_setup\\tentacle_setup.ino"'  # Path to Arduino sketch
+filepath        = '"C:\\Users\\Stefan Mucha\\arduino_python\\cont_readings\\cont_readings_i2c.ino"'  # Path to Arduino sketch
+filepath2        = '"C:\\Users\\Stefan Mucha\\arduino_python\\tentacle_setup\\tentacle_setup.ino"'  # Path to Arduino sketch
+
 
 # Subscript for finding the COM port where the arduino is connected. Only works with one arduino connected!
 wmi = win32com.client.GetObject("winmgmts:") #Find com port where Arduino is connected
@@ -14,6 +17,8 @@ for port in wmi.InstancesOf("Win32_SerialPort"):
     if "Arduino" in port.Name:
         comPort = ' --port ' + port.DeviceID
 
-command = "cmd /k arduino"+action+filepath+comPort    # Concatenate the single arguments into one command that is passed to the windows cmd.exe
+command = "cmd /k arduino"+action+filepath+comPort      # Concatenate the single arguments into one command that is passed to the windows cmd.exe
 
-subprocess.Popen(command, cwd=arduino_path)             # sets the arduino path as working directory in cmd.exe and executes the command
+subprocess.Popen(command, cwd=arduino_path)             # Set the arduino path as working directory in cmd.exe and executes the command
+
+#ser = serial.Serial(comPort, 9600)                      # Connect to Arduino via serial communication
